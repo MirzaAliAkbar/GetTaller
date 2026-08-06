5f120f22171d7ca9c204775dbe0851d737cface1
MirzaAliAkbar <AliAkbar@tutamail.com>
2026-08-06 09:24:23 +0500

    Fix missing imports in ai_coach_tab

--- lib/features/ai_coach/presentation/screens/ai_coach_tab.dart
+++ lib/features/ai_coach/presentation/screens/ai_coach_tab.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../core/theme/app_theme.dart';
 import '../../../../core/utils/constants.dart';
